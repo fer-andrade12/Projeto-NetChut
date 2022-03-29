c5e6c188e7c6dddeafc62b570b312ad460e3ba2b
fer-andrade12 <fer.developer@outlook.com>
2022-03-29 20:43:00 -0300

    Projeto criando em php com banco de dados

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco; Numero gerado free WHA, a senha que você criou para o banco, e o número novamanente do free WHA
+
+$conn = mysqli_connect('localhost', '367577', 'djd12123', '367577') or die('Erro ao conectar ao banco de dados');
